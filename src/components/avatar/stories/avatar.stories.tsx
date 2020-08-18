d331029005a713033bcd1486cf403211cb70caba
Gabriel Maracaípe <maracaipe@gmail.com>
2020-08-18 12:39:56 -0300

    Create avatar with different sizes story

--- src/components/avatar/stories/avatar.stories.tsx
+++ src/components/avatar/stories/avatar.stories.tsx
@@ -4,6 +4,7 @@ import { QuestionIcon } from '@chakra-ui/icons';
 import { BadgeIcon } from '../../../icons/badge';
 import { Stack } from '../../stack';
 import { Avatar, AvatarBadge, AvatarGroup } from '../';
+import { Wrap } from '@chakra-ui/core';
 
 export default {
   title: 'Avatar',
@@ -43,11 +44,31 @@ export const withBadge = () => (
 
 export const groupAvatar = () => (
   <AvatarGroup size="md" max={2}>
+    <Avatar name="Diego Gomes" src="https://bit.ly/2YeHB7z" />
     <Avatar name="Gabriel Maracaípe" src="https://bit.ly/2DU7Ngr" />
     <Avatar name="Ryan Florence" src="https://bit.ly/ryan-florence" />
-    <Avatar name="Segun Adebayo" src="https://bit.ly/sage-adebayo" />
     <Avatar name="Kent Dodds" src="https://bit.ly/kent-c-dodds" />
     <Avatar name="Prosper Otemuyiwa" src="https://bit.ly/prosper-baba" />
     <Avatar name="Christian Nwamba" src="https://bit.ly/code-beast" />
   </AvatarGroup>
+);
+
+export const withSizes = () => (
+  <Wrap>
+    <Avatar size="2xs" name="Dan Abrahmov" src="https://bit.ly/dan-abramov" />
+    <Avatar
+      size="xs"
+      name="Kola Tioluwani"
+      src="https://bit.ly/tioluwani-kolawole"
+    />
+    <Avatar size="sm" name="Kent Dodds" src="https://bit.ly/kent-c-dodds" />
+    <Avatar size="md" name="Ryan Florence" src="https://bit.ly/ryan-florence" />
+    <Avatar
+      size="lg"
+      name="Prosper Otemuyiwa"
+      src="https://bit.ly/prosper-baba"
+    />
+    <Avatar size="xl" name="Christian Nwamba" src="https://bit.ly/code-beast" />
+    <Avatar size="2xl" name="Diego Gomes" src="https://bit.ly/2YeHB7z" />
+  </Wrap>
 );
